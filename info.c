3e571a1943a62e212ab85058f35dca24b9ae7da5
srikanthpragada <srikanthpragada@gmail.com>
2021-09-25 17:42:56 +0530

    25-SEP

---
+++ info.c
@@ -0,0 +1,8 @@
+// Programs to print details
+#include <stdio.h>
+
+void main()
+{
+   printf("Srikanth Technologies\n");
+   printf("9059057000");
+}
---
+++ square.c
@@ -0,0 +1,17 @@
+// Program to display square of the given number
+// Date : 25-SEP-2021
+
+#include <stdio.h>
+
+void main()
+{
+  int num, square; // variables
+
+    printf("Enter a number :");
+    scanf("%d",&num);
+
+    square = num * num;
+    printf("%d", square);
+
+}
+
